1d58050aea7cb1d702484a7bfbc515daee74f4a1
Ion Dodon <iondodon2@gmail.com>
2019-09-17 20:32:08 +0300

    17 sep

--- lab1/client.py
+++ lab1/client.py
@@ -1,5 +1,16 @@
+from threading import Thread
 import requests
 import json
+import time
+
+data = {}
+threads = []
+token = ''
+
+
+def convert_and_store(mime_type):
+    print(mime_type)
+    return
 
 
 def get_token():
@@ -8,9 +19,11 @@ def get_token():
     json_str = response.content.decode('utf8')
     json_dict = json.loads(json_str)
 
+    global token
     for key, value in json_dict.items():
         if key == 'access_token':
-            return value
+            token = value
+            return token
 
 
 def parse(route):
@@ -20,13 +33,28 @@ def parse(route):
     json_str = response.content.decode('utf8')
     json_dict = json.loads(json_str)
 
-    links = json_dict['link']
-    for key in links.keys():
-        parse(links[key])
+    if 'mime_type' in json_dict.keys():
+        convert_and_store(json_dict['mime_type'])
+
+    for key in json_dict.keys():
+        if key == 'link':
+            for link_key in json_dict['link'].keys():
+                thrd = Thread(target=parse, args=[json_dict[key][link_key]])
+                threads.append(thrd)
+                thrd.start()
 
     print('\n')
 
 
-token = get_token()
-if token:
-    parse('/home')
+start_time = time.time()
+
+if get_token():
+    thread = Thread(target=parse, args=['/home'])
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+end_time = time.time()
+print(end_time - start_time)
