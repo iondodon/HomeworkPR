6b3a8f3a4a6f2494711ded8569e8fc44b1e70d9a
Ion Dodon <iondodon2@gmail.com>
2019-09-24 19:14:53 +0300

    lab 1

--- lab1/client.py
+++ lab1/client.py
@@ -1,24 +1,13 @@
 from threading import Thread
 import requests
 import json
-import time
-import xmltodict
+
+import xml.etree.ElementTree as ET
 import csv
 
 store = {}
 threads = []
 token = ''
-
-
-def convert_and_store(route, mime_type, json_dict):
-    print(mime_type)
-    data = json_dict['data']
-    if mime_type == 'application/xml':
-        store[route] = xmltodict.parse(data)
-    elif mime_type == 'text/csv':
-        store[route] = csv.reader(data)
-    # else:
-    #     store[route] = json.load(data)
 
 
 def get_token():
@@ -34,14 +23,39 @@ def get_token():
             return token
 
 
+def store_xml(xmldata_string):
+    with open('help_file', mode='w', newline='') as xmlfile:
+        xmlfile.write(xmldata_string)
+
+    with open('help_file', mode='r', newline='') as xmlfile:
+        tree = ET.parse(xmlfile)
+        root = tree.getroot()
+        print(root)
+
+
+def store_csv(csvdata_string):
+    with open('help_file', mode='w', newline='') as csvfile:
+        csvfile.write(csvdata_string)
+
+    with open('help_file', mode='r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row)
+
+
+def convert_and_store(route, mime_type, json_dict):
+    data = json_dict['data']
+    if mime_type == 'application/xml':
+        store_xml(data)
+    if mime_type == 'text/csv':
+        store_csv(data)
+
+
 def parse(route):
-    print(route)
     response = requests.get('http://localhost:5000' + route, headers={'X-Access-Token': token})
 
     json_str = response.content.decode('utf8')
     json_dict = json.loads(json_str)
-
-    print(json_str)
 
     if 'mime_type' in json_dict.keys():
         convert_and_store(route, json_dict['mime_type'], json_dict)
@@ -53,10 +67,6 @@ def parse(route):
                 threads.append(thrd)
                 thrd.start()
 
-    print('\n')
-
-
-start_time = time.time()
 
 if get_token():
     thread = Thread(target=parse, args=['/home'])
@@ -65,9 +75,3 @@ if get_token():
 
 for thread in threads:
     thread.join()
-
-end_time = time.time()
-print(end_time - start_time)
-print(len(store))
-
-print(store['/route/1/1'])
